1e8ca7613751257bf46c59f04ae2efcfed2c6347
Nick Logan <nlogan@maxmind.com>
2022-01-20 14:19:13 +0000

    Remove unnecessary sanitization

--- src/request/transaction.ts
+++ src/request/transaction.ts
@@ -117,16 +117,12 @@ export default class Transaction {
   private sanitizeKeys() {
     const sanitized = Object.assign({}, this) as any;
 
-    if (sanitized.creditCard != null) {
-      if (sanitized.creditCard.last4digits != null) {
-        sanitized.creditCard.last_digits = this.creditCard!.last4digits;
-        delete sanitized.creditCard.last4digits;
-      }
-
-      if (sanitized.creditCard.lastDigits != null) {
-        sanitized.creditCard.last_digits = this.creditCard!.lastDigits;
-        delete sanitized.creditCard.lastDigits;
-      }
+    if (
+      sanitized.creditCard &&
+      Object.prototype.hasOwnProperty.call(sanitized.creditCard, 'lastDigits')
+    ) {
+      sanitized.creditCard.last_digits = sanitized.creditCard.lastDigits;
+      delete sanitized.creditCard.lastDigits;
     }
 
     if (
