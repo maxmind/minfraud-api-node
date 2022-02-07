2caa0bbbe25c1911d6205c676be98b708daa1dcb
Fatih Aktas <akfatih2@gmail.com>
2022-02-07 14:20:41 -0700

    Add the rest of the left tests

--- src/webServiceClient.spec.ts
+++ src/webServiceClient.spec.ts
@@ -14,7 +14,6 @@ import {
 } from './index';
 import { Factors } from './response/models';
 import * as webRecords from './response/web-records';
-import { camelizeResponse } from './utils';
 
 const baseUrl = 'https://minfraud.maxmind.com';
 const nockInstance = nock(baseUrl);
@@ -777,140 +776,140 @@ describe('WebServiceClient', () => {
     });
   });
 
-  //   describe('reportTransaction', () => {
-  //     it('handles bare minimum request', () => {
-  //       const report = new TransactionReport({
-  //         ipAddress: '1.1.1.1',
-  //         tag: Constants.Tag.CHARGEBACK,
-  //       });
+  describe('reportTransaction', () => {
+    it('handles bare minimum request', () => {
+      const report = new TransactionReport({
+        ipAddress: '1.1.1.1',
+        tag: Constants.Tag.CHARGEBACK,
+      });
 
-  //       expect.assertions(1);
+      expect.assertions(1);
 
-  //       nockInstance
-  //         .post(fullPath('transactions/report'), report.toString())
-  //         .reply(204);
+      nockInstance
+        .post(fullPath('transactions/report'), report.toString())
+        .reply(204);
 
-  //       return expect(client.reportTransaction(report)).resolves.toBeUndefined();
-  //     });
+      return expect(client.reportTransaction(report)).resolves.toBeUndefined();
+    });
 
-  //     it('handles a "full" request', () => {
-  //       const report = new TransactionReport({
-  //         chargebackCode: 'foobar',
-  //         ipAddress: '1.1.1.1',
-  //         maxmindId: '1234',
-  //         minfraudId: '1234',
-  //         notes: 'hello world',
-  //         tag: Constants.Tag.CHARGEBACK,
-  //         transactionId: 'what the',
-  //       });
+    it('handles a "full" request', () => {
+      const report = new TransactionReport({
+        chargebackCode: 'foobar',
+        ipAddress: '1.1.1.1',
+        maxmindId: '1234',
+        minfraudId: '1234',
+        notes: 'hello world',
+        tag: Constants.Tag.CHARGEBACK,
+        transactionId: 'what the',
+      });
 
-  //       expect.assertions(1);
+      expect.assertions(1);
 
-  //       nockInstance
-  //         .post(fullPath('transactions/report'), report.toString())
-  //         .reply(204);
+      nockInstance
+        .post(fullPath('transactions/report'), report.toString())
+        .reply(204);
 
-  //       return expect(client.reportTransaction(report)).resolves.toBeUndefined();
-  //     });
-  //   });
+      return expect(client.reportTransaction(report)).resolves.toBeUndefined();
+    });
+  });
 
-  //   describe('error handling', () => {
-  //     const transaction = new Transaction({
-  //       device: new Device({
-  //         ipAddress: '1.1.1.1',
-  //       }),
-  //     });
+  describe('error handling', () => {
+    const transaction = new Transaction({
+      device: new Device({
+        ipAddress: '1.1.1.1',
+      }),
+    });
 
-  //     it('handles 5xx level errors', () => {
-  //       expect.assertions(1);
+    it('handles 5xx level errors', () => {
+      expect.assertions(1);
 
-  //       nockInstance
-  //         .post(fullPath('score'), score.request.basic)
-  //         .basicAuth(auth)
-  //         .reply(500);
+      nockInstance
+        .post(fullPath('score'), score.request.basic)
+        .basicAuth(auth)
+        .reply(500);
 
-  //       return expect(client.score(transaction)).rejects.toEqual({
-  //         code: 'SERVER_ERROR',
-  //         error: 'Received a server error with HTTP status code: 500',
-  //         url: baseUrl + fullPath('score'),
-  //       });
-  //     });
+      return expect(client.score(transaction)).rejects.toEqual({
+        code: 'SERVER_ERROR',
+        error: 'Received a server error with HTTP status code: 500',
+        url: baseUrl + fullPath('score'),
+      });
+    });
 
-  //     it('handles 3xx level errors', () => {
-  //       expect.assertions(1);
+    it('handles 3xx level errors', () => {
+      expect.assertions(1);
 
-  //       nockInstance
-  //         .post(fullPath('score'), score.request.basic)
-  //         .basicAuth(auth)
-  //         .reply(300);
+      nockInstance
+        .post(fullPath('score'), score.request.basic)
+        .basicAuth(auth)
+        .reply(300);
 
-  //       return expect(client.score(transaction)).rejects.toEqual({
-  //         code: 'HTTP_STATUS_CODE_ERROR',
-  //         error: 'Received an unexpected HTTP status code: 300',
-  //         url: baseUrl + fullPath('score'),
-  //       });
-  //     });
+      return expect(client.score(transaction)).rejects.toEqual({
+        code: 'HTTP_STATUS_CODE_ERROR',
+        error: 'Received an unexpected HTTP status code: 300',
+        url: baseUrl + fullPath('score'),
+      });
+    });
 
-  //     it('handles errors with unknown payload', () => {
-  //       expect.assertions(1);
+    it('handles errors with unknown payload', () => {
+      expect.assertions(1);
 
-  //       nockInstance
-  //         .post(fullPath('score'), score.request.basic)
-  //         .basicAuth(auth)
-  //         .reply(401, { foo: 'bar' });
+      nockInstance
+        .post(fullPath('score'), score.request.basic)
+        .basicAuth(auth)
+        .reply(401, { foo: 'bar' });
 
-  //       return expect(client.score(transaction)).rejects.toEqual({
-  //         code: 'INVALID_RESPONSE_BODY',
-  //         error: 'Received an invalid or unparseable response body',
-  //         url: baseUrl + fullPath('score'),
-  //       });
-  //     });
+      return expect(client.score(transaction)).rejects.toEqual({
+        code: 'INVALID_RESPONSE_BODY',
+        error: 'Received an invalid or unparseable response body',
+        url: baseUrl + fullPath('score'),
+      });
+    });
 
-  //     it('handles general http.request errors', () => {
-  //       const error = {
-  //         code: 'FOO_ERR',
-  //         message: 'some foo error',
-  //       };
+    it('handles general http.request errors', () => {
+      const error = {
+        code: 'FOO_ERR',
+        message: 'some foo error',
+      };
 
-  //       const expected = {
-  //         code: error.code,
-  //         error: error.message,
-  //         url: baseUrl + fullPath('score'),
-  //       };
+      const expected = {
+        code: error.code,
+        error: error.message,
+        url: baseUrl + fullPath('score'),
+      };
 
-  //       expect.assertions(1);
+      expect.assertions(1);
 
-  //       nockInstance
-  //         .post(fullPath('score'), score.request.basic)
-  //         .basicAuth(auth)
-  //         .replyWithError(error);
+      nockInstance
+        .post(fullPath('score'), score.request.basic)
+        .basicAuth(auth)
+        .replyWithError(error);
 
-  //       return expect(client.score(transaction)).rejects.toEqual(expected);
-  //     });
+      return expect(client.score(transaction)).rejects.toEqual(expected);
+    });
 
-  //     test.each`
-  //       status | code                       | error
-  //       ${400} | ${'IP_ADDRESS_INVALID'}    | ${'ip address invalid'}
-  //       ${400} | ${'IP_ADDRESS_REQUIRED'}   | ${'ip address required'}
-  //       ${400} | ${'IP_ADDRESS_RESERVED'}   | ${'ip address reserved'}
-  //       ${400} | ${'JSON_INVALID'}          | ${'invalid json'}
-  //       ${401} | ${'AUTHORIZATION_INVALID'} | ${'auth required'}
-  //       ${401} | ${'LICENSE_KEY_REQUIRED'}  | ${'license key required'}
-  //       ${401} | ${'USER_ID_REQUIRED'}      | ${'user id required'}
-  //       ${402} | ${'INSUFFICIENT_FUNDS'}    | ${'no money!'}
-  //       ${403} | ${'PERMISSION_REQUIRED'}   | ${'permission required'}
-  //     `('handles $code error', ({ code, error, status }) => {
-  //       nockInstance
-  //         .post(fullPath('score'), score.request.basic)
-  //         .basicAuth(auth)
-  //         .reply(status, { code, error });
-  //       expect.assertions(1);
+    test.each`
+      status | code                       | error
+      ${400} | ${'IP_ADDRESS_INVALID'}    | ${'ip address invalid'}
+      ${400} | ${'IP_ADDRESS_REQUIRED'}   | ${'ip address required'}
+      ${400} | ${'IP_ADDRESS_RESERVED'}   | ${'ip address reserved'}
+      ${400} | ${'JSON_INVALID'}          | ${'invalid json'}
+      ${401} | ${'AUTHORIZATION_INVALID'} | ${'auth required'}
+      ${401} | ${'LICENSE_KEY_REQUIRED'}  | ${'license key required'}
+      ${401} | ${'USER_ID_REQUIRED'}      | ${'user id required'}
+      ${402} | ${'INSUFFICIENT_FUNDS'}    | ${'no money!'}
+      ${403} | ${'PERMISSION_REQUIRED'}   | ${'permission required'}
+    `('handles $code error', ({ code, error, status }) => {
+      nockInstance
+        .post(fullPath('score'), score.request.basic)
+        .basicAuth(auth)
+        .reply(status, { code, error });
+      expect.assertions(1);
 
-  //       return expect(client.score(transaction)).rejects.toEqual({
-  //         code,
-  //         error,
-  //         url: baseUrl + fullPath('score'),
-  //       });
-  //     });
-  //   });
+      return expect(client.score(transaction)).rejects.toEqual({
+        code,
+        error,
+        url: baseUrl + fullPath('score'),
+      });
+    });
+  });
 });
